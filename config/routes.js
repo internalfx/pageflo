3394c083a190a8973f0086f22284e5777d6a861b
Bryan Morris <bryan@internalfx.com>
2020-02-20 15:48:36 -0700

    tidy

--- config/routes.js
+++ config/routes.js
@@ -1,25 +1,18 @@
 
 module.exports = {
-
   // Admin API
-
   'post /api/auth/getLink': 'loginController.getLink',
   'post /api/auth/login': 'loginController.login',
   'post /api/auth/logout': 'loginController.logout',
-  // 'post /api/auth/impersonate': 'loginController.impersonate',
-  // 'post /api/auth/unimpersonate': 'loginController.unimpersonate',
   'get /api/auth/user': 'loginController.user',
 
   'get /api/file/download/:filename*': 'api.fileController.download',
   'post /api/file/upload': 'api.fileController.upload',
 
   // Client API
-
   'get /api/client/file/download/:filename': 'client.fileController.download',
   'get /api/client/entry/list': 'client.entryController.list',
   'options /api/client/entry/list': 'client.entryController.list',
   'get /api/client/entry/show': 'client.entryController.show',
   'options /api/client/entry/show': 'client.entryController.show'
-
-  // 'get /assets/:path*': 'appController.asset'
 }
