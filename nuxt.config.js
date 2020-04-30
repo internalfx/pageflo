552076ceca65f0406eeea716342603a67f56beb5
Bryan Morris <bryan@internalfx.com>
2020-04-30 09:21:36 -0700

    use corejs3

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,38 +18,37 @@ module.exports = {
           login: { url: '/api/auth/login', method: 'post', propertyName: 'token' },
           logout: { url: '/api/auth/logout', method: 'post' },
           user: { url: '/api/auth/user', method: 'get', propertyName: 'user' }
-        },
-        tokenType: false
+        }
       }
+    },
+    token: {
+      prefix: 'pageflo.'
     },
     cookie: {
       options: {
         maxAge: 60 * 60 * 24 * 300
       }
     },
-    token: {
-      prefix: 'pageflo.'
-    },
-    localStorage: false,
+    localStorage: {},
     rewriteRedirects: true,
     fullPathRedirect: true
   },
   axios: {},
   build: {
-    // babel: {
-    //   presets: function ({ isServer }) {
-    //     return [
-    //       [
-    //         require.resolve('@nuxt/babel-preset-app'),
-    //         // require.resolve('@nuxt/babel-preset-app-edge'), // For nuxt-edge users
-    //         {
-    //           buildTarget: isServer ? 'server' : 'client',
-    //           corejs: { version: 3 }
-    //         }
-    //       ]
-    //     ]
-    //   }
-    // },
+    babel: {
+      presets: function ({ isServer }) {
+        return [
+          [
+            require.resolve('@nuxt/babel-preset-app'),
+            // require.resolve('@nuxt/babel-preset-app-edge'), // For nuxt-edge users
+            {
+              buildTarget: isServer ? 'server' : 'client',
+              corejs: { version: 3 }
+            }
+          ]
+        ]
+      }
+    },
     extend: function (config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.devtool = 'inline-source-map'
@@ -80,7 +79,6 @@ module.exports = {
     ]
   },
   mode: 'spa',
-  // modern: 'client',
   loading: { color: '#888888' },
   modules: [
     '@nuxtjs/apollo',
